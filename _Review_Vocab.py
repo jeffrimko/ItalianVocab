98ce7c42818e0160f543d48b184809fda0c58ac7
jeffrimko <jeffrimko@gmail.com>
2018-04-22 11:06:47 -0400

    First draft of refactor, moving code to be not tied to a specific language.

--- _Review_Vocab.py
+++ _Review_Vocab.py
@@ -6,7 +6,9 @@ import os
 import string
 import random; random.seed()
 import unicodedata
+from threading import Thread, Lock
 
+import related
 import qprompt as q
 from auxly.filesys import File, delete
 from gtts import gTTS
@@ -20,6 +22,97 @@ MISSED_VOCAB = "__temp-vocab_to_review.txt"
 
 RANDOM_VOCAB = "__temp-random_vocab.txt"
 
+TALK_LOCK = Lock()
+
+##==============================================================#
+## SECTION: Global Definitions                                  #
+##==============================================================#
+
+@related.immutable
+class LanguageName(object):
+    short = related.StringField(default="en")
+    full = related.StringField(default="English")
+
+@related.immutable
+class LanguageInfo(object):
+    name = related.ChildField(LanguageName)
+    talk = related.BooleanField(default=False)
+    hint = related.IntegerField(default=0)
+
+@related.immutable
+class TextInfo(object):
+    text = related.StringField()
+    lang = related.ChildField(LanguageInfo)
+
+@related.immutable
+class PracticeConfig(object):
+    lang1 = related.ChildField(LanguageInfo)
+    lang2 = related.ChildField(LanguageInfo)
+    num = related.IntegerField(default=10)
+    path = related.StringField(default="")
+    redo = related.BooleanField(default=False)
+    swap = related.BooleanField(default=False)
+
+@related.immutable
+class UtilConfig(object):
+    lang1 = related.ChildField(LanguageInfo)
+    lang2 = related.ChildField(LanguageInfo)
+    redo = related.BooleanField(default=False)
+    path = related.StringField(default=".")
+    num = related.IntegerField(default=10)
+
+class Practice(object):
+    def __init__(self, config):
+        self.config = config
+        self.miss = set()
+        self.okay = set()
+
+    def start(self):
+        self.miss = set()
+        self.okay = set()
+        path = self.config.path or get_file()
+        lines = [line.strip() for line in File(path).readlines() if line][:self.config.num]
+        random.shuffle(lines)
+        try:
+            for (num, line) in enumerate(lines):
+                q.hrule()
+                q.echo("%s of %s" % (num+1, len(lines)))
+                self._ask(line)
+        except:
+            q.echo()
+        fmiss = File(MISSED_VOCAB)
+        for miss in self.miss:
+            fmiss.append(miss + "\n")
+
+    def _ask(self, line):
+        line = unicodedata.normalize('NFKD', line).encode('ascii','ignore').decode("utf-8").strip()
+        if not line: return
+        qst = TextInfo(line.split(";")[0], self.config.lang1)
+        ans = TextInfo(line.split(";")[1], self.config.lang2)
+        if self.config.swap:
+            ans,qst = qst,ans
+        msg = qst.text
+        if ans.lang.hint:
+            msg += " (%s)" % hint(ans.text, ans.lang.hint)
+        talk_qst = run_once(talk)
+        while True:
+            q.alert(msg)
+            if qst.lang.talk:
+                talk_qst(qst.text, qst.lang.name.short)
+            rsp = q.ask_str("").lower().strip()
+            ok = [x.lower().strip() for x in ans.text.split("(")[0].split("/")]
+            if rsp in ok:
+                q.echo("[CORRECT] " + ans.text)
+                self.okay.add(line)
+            else:
+                q.error(ans.text)
+                self.miss.add(line)
+                if self.config.redo:
+                    continue
+            if ans.lang.talk:
+                talk(ans.text, ans.lang.name.short, wait=True)
+            return
+
 ##==============================================================#
 ## SECTION: Function Definitions                                #
 ##==============================================================#
@@ -28,7 +121,26 @@ stridxrep = lambda s, i, r: "".join([(s[x] if x != i else r) for x in range(len(
 
 randstr = lambda length: "".join(random.choice(string.ascii_lowercase) for _ in range(length))
 
-def ask_file(msg="File to review"):
+#: from https://stackoverflow.com/questions/4103773/efficient-way-of-having-a-function-only-execute-once-in-a-loop
+def run_once(f):
+    def wrapper(*args, **kwargs):
+        if not wrapper.has_run:
+            wrapper.has_run = True
+            return f(*args, **kwargs)
+    wrapper.has_run = False
+    return wrapper
+
+def get_file():
+    menu = q.Menu()
+    menu.add("f", "Select file")
+    menu.add("r", "Random vocab")
+    choice = menu.show()
+    if "f" == choice:
+        return ask_file()
+    else:
+        return make_random_file()
+
+def ask_file(msg="File to review (blank to list)"):
     """Prompts user for a file to review. Returns the file name."""
     path = q.ask_str(msg, blk=True)
     if not path or not os.path.isfile(path):
@@ -36,12 +148,18 @@ def ask_file(msg="File to review"):
         path = q.enum_menu(vfiles).show(returns="desc", limit=20)
     return path
 
-def pronounce_it(text, slow=False):
-    """Pronouces the given text in Italian."""
-    fpath = f"__temp-pronounce_it-{randstr(6)}.mp3"
-    gTTS(text=text, lang="it", slow=slow).save(fpath)
-    playsound(fpath)
-    delete(fpath)
+def talk(text, lang, slow=False, wait=False):
+    def _talk():
+        """Pronouces the given text in Italian."""
+        with TALK_LOCK:
+            fpath = f"__temp-talk-{randstr(6)}.mp3"
+            gTTS(text=text, lang=lang, slow=slow).save(fpath)
+            playsound(fpath)
+            delete(fpath)
+    t = Thread(target=_talk)
+    t.start()
+    if wait:
+        t.join()
 
 def make_random_file(num=20):
     vocabs = []
@@ -57,70 +175,7 @@ def make_random_file(num=20):
     File(RANDOM_VOCAB).write("\n".join(vocabs))
     return RANDOM_VOCAB
 
-def get_file():
-    menu = q.Menu()
-    menu.add("f", "Select file")
-    menu.add("r", "Random vocab")
-    choice = menu.show()
-    if "f" == choice:
-        return ask_file()
-    else:
-        return make_random_file()
-
-def practice_it2en():
-    path = get_file()
-    redo = q.ask_yesno("Redo missed answers?")
-    lines = [line.strip() for line in File(path).readlines() if line]
-    random.shuffle(lines)
-    try:
-        for line in lines:
-            line = unicodedata.normalize('NFKD', line).encode('ascii','ignore').decode("utf-8").strip()
-            if not line: continue
-            en,it = line.split(";")
-            pronounce_it(it)
-            while True:
-                ans = q.ask_str(it).lower().strip()
-                ok = [x.lower().strip() for x in en.split("(")[0].split("/")]
-                if ans in ok:
-                    q.echo("CORRECT! " + en)
-                    break
-                else:
-                    q.error("%s" % (en))
-                    fmiss.write(line + "\n", "a")
-                    if not redo:
-                        break
-    except:
-        print()
-
-def practice_en2it():
-    path = get_file()
-    hint = q.ask_int("Number of hints letters", dft=0)
-    redo = q.ask_yesno("Redo missed answers?")
-    lines = [line.strip() for line in File(path).readlines() if line]
-    random.shuffle(lines)
-    try:
-        for line in lines:
-            line = unicodedata.normalize('NFKD', line).encode('ascii','ignore').decode("utf-8").strip()
-            if not line: continue
-            en,it = line.split(";")
-            msg = en
-            if hint > 0:
-                msg += " (%s)" % hint_vocab(it, hint)
-            while True:
-                ans = q.ask_str(msg).lower().strip()
-                ok = [x.lower().strip() for x in it.split("(")[0].split("/")]
-                if ans in ok:
-                    q.echo("CORRECT! " + it)
-                    break
-                else:
-                    q.error("%s" % (it))
-                    fmiss.write(line + "\n", "a")
-                    if not redo:
-                        break
-    except:
-        print()
-
-def hint_vocab(vocab, hintnum, skipchars=" /'"):
+def hint(vocab, hintnum, skipchars=" /'"):
     """Returns the given string with all characters expect the given hint
     number replaced with an asterisk. The given skip characters will be
     excluded."""
@@ -140,14 +195,34 @@ def hint_vocab(vocab, hintnum, skipchars=" /'"):
             hint = stridxrep(hint, i, "*")
     return hint
 
+def main():
+    config = related.to_model(UtilConfig, related.from_yaml(File("config.yaml").read()))
+    try:
+        show_main_menu(config)
+    except:
+        pass
+
+def show_main_menu(config):
+    menu = q.Menu()
+    p1 = PracticeConfig(
+            lang1 = config.lang1,
+            lang2 = config.lang2,
+            redo = config.redo,
+            num = config.num)
+    p2 = PracticeConfig(
+            lang1 = config.lang1,
+            lang2 = config.lang2,
+            redo = config.redo,
+            num = config.num,
+            swap = True)
+    menu.add("1", "%s to %s" % (config.lang1.name.full, config.lang2.name.full), Practice(p1).start)
+    menu.add("2", "%s to %s" % (config.lang2.name.full, config.lang1.name.full), Practice(p2).start)
+    menu.add("d", "Delete missed words to review", delete, [MISSED_VOCAB])
+    menu.main(loop=True)
+
 ##==============================================================#
 ## SECTION: Main Body                                           #
 ##==============================================================#
 
 if __name__ == '__main__':
-    fmiss = File(MISSED_VOCAB)
-    menu = q.Menu()
-    menu.add("1", "English to Italian", practice_en2it)
-    menu.add("2", "Italian to English", practice_it2en)
-    menu.add("d", "Delete missed words to review", delete, [MISSED_VOCAB])
-    menu.main(loop=True)
+    main()
